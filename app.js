bd07b7355abd666369546b728cca9bb9083c46b1
mkingrose <66294127+mkingrose@users.noreply.github.com>
2020-12-05 11:33:59 -0500

    logos added to master

--- app.js
+++ app.js
@@ -56,11 +56,21 @@ function getdata(tm) {
       "ATL": "Red", "BRK": "Black", "BOS": "Green", "CHO": "purple", "CHI": "red", "CLE": "maroon", "DAL": "blue", "DEN": "navy", "DET": "blue", "GSW": "yellow", "HOU": "red", "IND": "blue", "LAC": "Red", "LAL": "Purple", "MEM": "blue", "MIA": "maroon",
       "MIL": "green", "MIN": "navy", "NOP": "blue", "NYK": "FF7F03", "OKC": "light blue", "ORL": "blue", "PHI": "blue", "PHO": "FF7F03", "POR": "red", "SAC": "purple", "SAS": "gray", "TOR": "red", "UTA": "yellow", "WAS": "red"
     };
+    var logos = {
+      "ATL": "Resources/NBA_Logos-master/atlanta.png", "BRK": "Resources/NBA_Logos-master/brooklyn.png", "BOS": "Resources/NBA_Logos-master/boston.pngGreen", "CHO": "Resources/NBA_Logos-master/charlotte.png", "CHI": "Resources/NBA_Logos-master/chicago.png", "CLE": "Resources/NBA_Logos-master/cleveland.png", "DAL": "Resources/NBA_Logos-master/dallas.png", "DEN": "Resources/NBA_Logos-master/denver.png", "DET": "Resources/NBA_Logos-master/detroit.png", "GSW": "Resources/NBA_Logos-master/golden_state.png", "HOU": "Resources/NBA_Logos-master/houston.png", "IND": "Resources/NBA_Logos-master/indiana.png", "LAC": "Resources/NBA_Logos-master/la_clippers.png", "LAL": "Resources/NBA_Logos-master/la_lakers.png", "MEM": "Resources/NBA_Logos-master/memphis.png", "MIA": "Resources/NBA_Logos-master/miami.png",
+      "MIL": "Resources/NBA_Logos-master/milwaukee.png", "MIN": "Resources/NBA_Logos-master/minnesota.png", "NOP": "Resources/NBA_Logos-master/new_orleans.png", "NYK": "Resources/NBA_Logos-master/new_york.png", "OKC": "Resources/NBA_Logos-master/oklahoma_city.png", "ORL": "Resources/NBA_Logos-master/orlando.png", "PHI": "Resources/NBA_Logos-master/philadelphia.png", "PHO": "Resources/NBA_Logos-master/phoenix.png", "POR": "Resources/NBA_Logos-master/portland.png", "SAC": "Resources/NBA_Logos-master/sacramento.png", "SAS": "Resources/NBA_Logos-master/san_antonio.png", "TOR": "Resources/NBA_Logos-master/toronto.png", "UTA": "Resources/NBA_Logos-master/utah.png", "WAS": "Resources/NBA_Logos-master/washington.png"
+    };
     console.log(colors);
     var teamList = salary_test.map(data => data.tm)[0];
     console.log(teamList);
     testing2 = colors[teamList];
+    testing3 = logos[teamList];
     console.log(testing2);
+
+    const domElement = d3.select(".atlanta-container");
+    domElement.html("");
+    domElement.append("img")
+      .attr('src', testing3);
 
 
     for (i = 1; i < player_list.length; i++) {
